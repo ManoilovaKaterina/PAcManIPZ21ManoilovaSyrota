3d09fca56277656a6d189865f8501dcb5642b893
LinaSyrota <angelinasirotaalex@gmail.com>
2023-03-03 21:23:48 +0200

    основний клас гри

---
+++ PacMan.py
@@ -0,0 +1,19 @@
+from GameInit import *
+from Field import *
+from Player import *
+from StaticObjects import *
+
+if __name__ == "__main__":
+    UniSize = 32
+    game = MazeAndPathController()
+    size = game.size
+    gameInit = GameInit(size[0] * UniSize, size[1] * UniSize)
+
+    for y, row in enumerate(game.numpy_maze):
+        for x, column in enumerate(row):
+            if column == 0:
+                gameInit.AddWall(Wall(gameInit, x, y, UniSize))
+
+    pacman = Player(gameInit, 32, 32, UniSize)
+    gameInit.AddPacman(pacman)
+    gameInit.MainLoop(120)
