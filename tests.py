7613a3c9e99e10994bfe10892cd89a33e467d33c
ManoilovaKaterina <katya.manoylova@gmail.com>
2023-03-23 16:08:50 +0200

    тести

--- tests.py
+++ tests.py
@@ -4,45 +4,56 @@ from GameInit import *
 from PacMan import *
 
 def test_path2():
-    print("test path")
-    assert MazeAndPathController(diffEasy).p.get_path(11, 0, 13, 1) == [(1, 11), (2, 11), (3, 11), (4, 11), (5, 11), (6, 11), (6, 12), (6, 13), (5, 13), (4, 13), (3, 13), (2, 13), (1, 13)]
+    expected = [(1, 11), (2, 11), (3, 11), (4, 11), (5, 11), (6, 11), (6, 12), (6, 13), (5, 13), (4, 13), (3, 13), (2, 13), (1, 13)]
+    assert MazeAndPathController(diffEasy).p.get_path(11, 0, 13, 1) == expected
 
 def test_arrays3():
-    print("test field")
     assert MazeAndPathController(diffEasy).ascii_maze == diffEasy
 
-def test_coords7():
-    print("test coords")
+def test_maze_t_screen7():
     assert MazeToScreen((10, 10)) == (320, 320)
 
-def test_coords8():
-    print("test coords")
+def test_screen_to_maze8():
     assert ScreenToMaze((320, 320)) == (10, 10)
 
-pacman_game = MazeAndPathController(diffEasy)
-size = pacman_game.size
-UniSize = 32
+global testghost
+global testpacman
 
-gameInit = GameInit(size[0] * UniSize, 800)
+def initGameForTests():
+    global testghost
+    global testpacman
+    pacman_game = MazeAndPathController(diffEasy)
+    size = pacman_game.size
+    UniSize = 32
 
-for y, row in enumerate(pacman_game.numpy_maze):
-    for x, column in enumerate(row):
-        if column == 0:
-            gameInit.AddWall(Wall(gameInit, x, y, UniSize))
+    gameInit = GameInit(size[0] * UniSize, 800)
 
-translated = MazeToScreen(pacman_game.ghost_spawns[0])
-ghost = Ghost(gameInit, translated[0], translated[1], UniSize, pacman_game, GhostColors[0])
-gameInit.AddGhost(ghost)
+    for y, row in enumerate(pacman_game.numpy_maze):
+        for x, column in enumerate(row):
+            if column == 0:
+                gameInit.AddWall(Wall(gameInit, x, y, UniSize))
+
+    translated = MazeToScreen(pacman_game.ghost_spawns[0])
+    testghost = Ghost(gameInit, translated[0], translated[1], UniSize, pacman_game, GhostColors[0])
+    gameInit.AddGhost(testghost)
+
+    testpacman = Player(gameInit, translated[0], translated[1], UniSize)
+    gameInit.AddPacman(testpacman)
+    # gameInit.isChasing = True
+    # gameInit.MainLoop(120)
+
+initGameForTests()
 
 def test_nextloc13():
-    ghost.locationQueue.append((3, 2))
-    assert ghost.GetNextLocation() == (3, 2)
+    testghost.locationQueue.append((3, 2))
+    assert testghost.GetNextLocation() == (3, 2)
 
-def test_ghost_move4():
-    preLoc = ghost.getPosition()
+@pytest.mark.parametrize("dir", [Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT])
+def test_ghost_move4(dir):
+    testghost.setPosition(6, 11)
+    preLoc = testghost.getPosition()
     
-    ghost.Move(Direction.UP)
-    dir = Direction.UP
+    testghost.Move(dir)
 
     if dir == Direction.UP:
         preLoc = (preLoc[0], preLoc[1] - 1)
@@ -53,11 +64,11 @@ def test_ghost_move4():
     elif dir == Direction.RIGHT:
         preLoc = (preLoc[0] + 1, preLoc[1])
 
-    assert ghost.getPosition() == preLoc
+    assert testghost.getPosition() == preLoc
 
 def test_collides14():
-    ghost.setPosition(ghost.spawnPoint[0], ghost.spawnPoint[1]-1)
-    assert ghost.CheckCollision(Direction.LEFT) == (True, (415, 351))
+    testghost.setPosition(testghost.spawnPoint[0], testghost.spawnPoint[1]-1)
+    assert testghost.CheckCollision(Direction.LEFT) == (True, (415, 351))
 
 def test_dir15():
-    assert ghost.DirectionToNextTarget() == Direction.NONE
+    assert testghost.DirectionToNextTarget() == Direction.NONE
